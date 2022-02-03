1039c32a84e9d4a2acf1df777de74c93f3644ea7
azamat7087 <azaza7087@gmail.com>
2022-02-03 14:15:11 +0600

    First commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from core.db import Base
+from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Post(Base):
+    __tablename__ = "blog_posts"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    title = Column(String(length=100), )
+    text = Column(Text, )
+    user = Column(Integer, ForeignKey("user.id"))
+    user_id = relationship("User")
+    date = Column(DateTime)
+
